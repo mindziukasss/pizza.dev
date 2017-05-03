97dc2d64877352092c1e846b9899c52949b7613c
Mindaugas Bernotas <mindaugasbernotas2@gmail.com>
2017-05-03 13:06:10 +0300

    added packagist ramsey/uuid and base model

---
+++ app/Models/PZBaseModel.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Code Academy
+ * Date: 5/3/2017
+ * Time: 1:01 PM
+ */
+
+namespace App\BaseModels;
+
+
+class PZBaseModel extends Model
+{
+    use SoftDeletes;
+
+    public $incrementing = false;
+
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+
+            $model->{$model->getKeyName()} = (string)$model->generateNewId();
+        });
+    }
+
+    /**
+     *
+     * @return \Ramsey\Uuid\UuidInterface
+     */
+    public function generateNewId()
+    {
+        if (isset($this->attributes['id'])) {
+            return $this->attributes['id'];
+        }
+
+        return Uuid::uuid4();
+    }
+}
